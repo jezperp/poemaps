44a89cee9418a975a52f289f2c44d461a6b7ab8a
Jesper Pettersson <jesper.pettersson@creo.se>
2023-10-24 00:44:14 +0200

    Added logo and favicon

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <header>
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+        <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="169" height="40" />
     </header>
     <RouterView />
 </template>
