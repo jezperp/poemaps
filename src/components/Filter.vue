83f5db90aa11377e5c87f08bf007e608445d281a
Jesper Pettersson <jesper.pettersson@creo.se>
2023-10-30 08:38:20 +0100

    Moved out filter to separate component

---
+++ src/components/Filter.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="filter">
+        <div class="row align-items-center">
+            <div class="col">
+                <input  @keyup="filter"
+                        type="text"
+                        class="form-control"
+                        placeholder="Type to search for a map...">
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+    const emit = defineEmits(['filter'])
+    function filter(e) {
+        emit('filter', e.target.value)
+    }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,17 @@
 <template>
     <main>
-        <div class="container px-4">
-            <div class="row align-items-center">
-                <div class="col">
-                    <input  v-model="filtered"
-                            type="text"
-                            class="form-control"
-                            placeholder="Type to search for a map...">
-                </div>
-            </div>
-        </div>
-        <div class="container px-3">
+        <div class="container">
+            <Filter @filter="filter" />
             <Maps :filter="filtered" />
         </div>
     </main>
 </template>
 <script setup>
     import { ref } from 'vue'
+    import Filter from '@/components/Filter.vue'
     import Maps from '@/components/Maps.vue'
     let filtered = ref(null)
+    function filter(e) {
+        filtered.value = e
+    }
 </script>
