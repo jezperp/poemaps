7b94fb12d9010cdce3f7f3b977bf2cf561640a34
Jesper Pettersson <jesper.pettersson@creo.se>
2023-11-01 01:23:48 +0100

    Added default base url

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import './assets/styles.scss'
 
+import axios from 'axios'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+
+axios.defaults.baseURL = `https://firestore.googleapis.com/v1/projects/poemaps-c9dcb/databases/(default)/documents/`
 
 const app = createApp(App)
 
